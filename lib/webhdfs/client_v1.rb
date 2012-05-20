c7f3b36e38d32f5cddbfd239b9555037271b6f1e
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-05-20 18:37:38 +0900

    fix to fill [] instead of nil

--- lib/webhdfs/client_v1.rb
+++ lib/webhdfs/client_v1.rb
@@ -168,7 +168,7 @@ module WebHDFS
     # end
 
     def check_options(options, optdecl=[])
-      ex = options.keys - optdecl
+      ex = options.keys - (optdecl || [])
       raise ArgumentError, "no such option: #{ex.keys.join(' ')}" unless ex.empty?
     end
 
