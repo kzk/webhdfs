8537ce42d8b97f49bf153371c630acebdff9f22a
Grisha Trubetskoy <grisha.trubetskoy@livingsocial.com>
2012-09-25 18:45:34 -0400

    backport to ruby 1.8.7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'rubygems'
 require 'rr'
 require 'test/unit'
 class Test::Unit::TestCase
