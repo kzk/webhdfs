a4f639faacbeb28668c7ac1c762d58fb33d54a68
Jorge Israel Peña <jorge@factual.com>
2019-01-09 16:11:34 -0800

    Migrate HDFS → InnerClient tests

---
+++ spec/lib/webhdfs/factual/inner_client_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../../../lib/webhdfs/factual'
+
+include WebHDFS::Factual
+
+API_HOST = 'http://localhost'
+DEFAULT_NAMENODE = 'default'
+
+describe InnerClient do
+  describe '#detect_namenode' do
+    it 'returns the correct namenode' do
+      stub_request(:get, /localhost/).to_return(body: fixture('namenode_status.json'))
+      expect(described_class.detect_namenode(API_HOST, DEFAULT_NAMENODE)).to eq('localhost')
+    end
+
+    it 'returns the default namenode when request fails' do
+      stub_request(:get, /localhost/).to_return(status: 500)
+      expect(described_class.detect_namenode(API_HOST, DEFAULT_NAMENODE)).to eq(DEFAULT_NAMENODE)
+    end
+  end
+end
