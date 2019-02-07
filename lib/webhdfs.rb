3cc6c1f084b4c141cb61394275551444419d7f00
Jorge Israel Peña <jorge@factual.com>
2019-02-07 15:36:24 -0800

    Also remove require of simple.rb

--- lib/webhdfs.rb
+++ lib/webhdfs.rb
@@ -1,5 +1,4 @@
 require File.join(File.dirname(__FILE__), 'webhdfs', 'null_logger.rb')
 require File.join(File.dirname(__FILE__), 'webhdfs', 'backport.rb')
 require File.join(File.dirname(__FILE__), 'webhdfs', 'client.rb')
-require File.join(File.dirname(__FILE__), 'webhdfs', 'simple.rb')
 require File.join(File.dirname(__FILE__), 'webhdfs', 'api_connection.rb')
