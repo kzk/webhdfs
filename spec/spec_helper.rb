4b28fdb656c9815f11f34d9dfe799819446bce22
Jorge Israel Peña <jorge@factual.com>
2019-02-08 10:15:37 -0800

    Reintroduce environment variable reading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow: [/\/localhost/, /\/hdfs-dev\./, /\/webhdfs\//])
 
-require_relative '../test/environment'
+JMX = ENV['JMX'] || 'http://localhost'
+DEFAULT_NAMENODE = ENV['DEFAULT_NAMENODE'] || 'localhost'
+TEST_PATH = ENV['TEST_PATH']
+
+if TEST_PATH.nil? || TEST_PATH.empty?
+  raise "Must explicitly set a TEST_PATH to run tests within"
+end
 
 def fixture_path
   File.expand_path("./spec/fixtures")
